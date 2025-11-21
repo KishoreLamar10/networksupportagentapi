b7111ef3197553dfc40e2030a6d2d16a50669d15
Kishorekumar Avudai Nayagam <kishorekumaravudainayagam@Kishorekumars-MacBook-Air-3.local>
2025-11-21 13:01:21 -0500

    api changes

--- app/api/server.py
+++ app/api/server.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # TEMPORARY - allow all during development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
